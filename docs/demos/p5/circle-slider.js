75e3fb8c0f3968ed4b2f411018ffef1cb9178ff5
dmccreary <dan.mccreary@gmail.com>
2023-07-04 10:25:27 -0500

    updated content

--- docs/demos/p5/circle-slider.js
+++ docs/demos/p5/circle-slider.js
@@ -1,16 +1,17 @@
-// single slider with text feedback
+// draw a circle and use a slider to change the radius
 function setup() {
     const canvas = createCanvas(400, 400);
     canvas.parent('canvas-container');
-    radiusSlider = createSlider(0, 400, 60, 1);
+    radiusSlider = createSlider(0, 200, 60, 1);
     radiusSlider.position(70, 0);
     radiusSlider.style('width', '320px');
   }
   
   function draw() {
-    background('white');
-    let val = radiusSlider.value();
+    background('220');
+    let radius = radiusSlider.value();
     fill('blue');
-    text('Radius: ' + val, 0, 15);
-    circle(width/2, height/2, val);
+    circle(width/2, height/2, radius*2);
+    fill('black');
+    text('Radius: ' + radius, 0, 15);
   }

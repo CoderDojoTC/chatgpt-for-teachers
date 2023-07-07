97b936ce59e55ebbee00feeeb6b935841358437f
dmccreary <dan.mccreary@gmail.com>
2023-07-07 07:37:19 -0500

    Deployed 6aa3ac9 with MkDocs version: 1.3.1

--- demos/p5/slider.js
+++ demos/p5/slider.js
@@ -1,20 +1,26 @@
-
-let simpleSlider;
+// p5.js sketch to draw a blue circle with a slider to change the radius
+// Generate by ChatGPT
+let radiusSlider;
+let circleRadius = 100; // default value
 
 function setup() {
-  createCanvas(400, 400);
-  // min, max, default, increment
-  simpleSlider = createSlider(0, 360, 100, 1);
-  // x and y position
-  simpleSlider.position(70, 10);
-  // how wide the slider should be
-  simpleSlider.style('width', '330px');
+    const canvas = createCanvas(400, 400);
+    canvas.parent('canvas-container');
+    radiusSlider = createSlider(0, 200, circleRadius);
+    radiusSlider.position(20, 370);
 }
 
 function draw() {
-  background(220);
-  let val = simpleSlider.value();
-  fill('black');
-  circle(width/2, height/2, val);
-  text('Value: ' + val, 5, 15);
+    background(220);
+
+    // Update the circle radius based on slider value
+    circleRadius = radiusSlider.value();
+
+    // Draw the circle
+    fill(0, 0, 255); // blue color
+    ellipse(width / 2, height / 2, circleRadius * 2, circleRadius * 2);
+
+    // Draw the label and the current radius value
+    fill(0); // black color for the text
+    text("Radius: " + circleRadius, radiusSlider.x + radiusSlider.width + 10, 385);
 }

72b15fce7750596fa234fb1e5c0fcb249a7d7979
dmccreary <dan.mccreary@gmail.com>
2023-07-14 15:07:33 -0500

    Deployed 098fb21 with MkDocs version: 1.3.1

--- demos/p5/circle-slider.js
+++ demos/p5/circle-slider.js
@@ -3,7 +3,8 @@ function setup() {
     const canvas = createCanvas(400, 400);
     canvas.parent('canvas-container');
     radiusSlider = createSlider(0, 200, 60, 1);
-    radiusSlider.position(70, 0);
+    // position 20 up from the bottom
+    radiusSlider.position(100, height - 20);
     radiusSlider.style('width', '320px');
   }
   
@@ -13,5 +14,5 @@ function setup() {
     fill('blue');
     circle(width/2, height/2, radius*2);
     fill('black');
-    text('Radius: ' + radius, 0, 15);
+    text('Radius: ' + radius, 10, height - 20);
   }

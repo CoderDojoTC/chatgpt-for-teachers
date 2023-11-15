b86b7155e844333e6ba505e3c26004cca8070b2c
Dan Mccreary <Dan.McCreary@gmail.com>
2023-11-14 22:31:59 -0600

    Deployed 9d8c160 with MkDocs version: 1.5.3

--- demos/p5/supply-and-demand.js
+++ demos/p5/supply-and-demand.js
@@ -1,57 +1,86 @@
-// Price variable
-var price = 0;
+let width = 400;
+let height = 400;
+let margin = 50; // side around the plot
+let priceSlider;
+let demandCurve;
+let maxPrice = 200;
+let maxQuantity = 200;
+let price = 100;
+let quantity = 100;
+let labelValueMargin = 140
 
-// Function to draw the chart
-function draw() {
-  // Get the current value of the slider
-  price = parseInt(document.getElementById('slider').value);
-
-  // Clear previous chart
-  var chart = document.getElementById('chart');
-  while (chart.firstChild) {
-    chart.removeChild(chart.firstChild);
-  }
-
-  // Create the axes
-  var xAxis = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-  xAxis.setAttribute('x1', '50');
-  xAxis.setAttribute('y1', '350');
-  xAxis.setAttribute('x2', '450');
-  xAxis.setAttribute('y2', '350');
-  xAxis.setAttribute('stroke', 'black');
-  xAxis.setAttribute('stroke-width', '2');
-  chart.appendChild(xAxis);
-
-  var yAxis = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-  yAxis.setAttribute('x1', '50');
-  yAxis.setAttribute('y1', '50');
-  yAxis.setAttribute('x2', '50');
-  yAxis.setAttribute('y2', '350');
-  yAxis.setAttribute('stroke', 'black');
-  yAxis.setAttribute('stroke-width', '2');
-  chart.appendChild(yAxis);
-
-  // Create the demand curve
-  var demandCurve = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-  var demandPath = 'M50,350 L' + price + ',50 L450,350';
-  demandCurve.setAttribute('d', demandPath);
-  demandCurve.setAttribute('fill', 'none');
-  demandCurve.setAttribute('stroke', 'blue');
-  demandCurve.setAttribute('stroke-width', '2');
-  chart.appendChild(demandCurve);
-
-  // Create the supply curve
-  var supplyCurve = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-  var supplyPath = 'M50,50 L' + price + ',350 L450,50';
-  supplyCurve.setAttribute('d', supplyPath);
-  supplyCurve.setAttribute('fill', 'none');
-  supplyCurve.setAttribute('stroke', 'red');
-  supplyCurve.setAttribute('stroke-width', '2');
-  chart.appendChild(supplyCurve);
+function setup() {
+  const canvas = createCanvas(width, height);
+  // canvas.parent('canvas-container');
+  textSize(16);
+  
+  // Create a slider element for price adjustment
+  priceSlider = createSlider(0, maxPrice, price);
+  priceSlider.position(labelValueMargin, height - 35);
+  priceSlider.style('width', width - labelValueMargin - 20 + 'px');
+  
+  // Define the demand curve as a function
+  demandCurve = (p) => maxQuantity - p;
 }
 
-// Attach the draw function to the slider's onchange event
-document.getElementById('slider').onchange = draw;
-
-// Initial chart draw
-draw();
+function draw() {
+  background(240);
+  
+  // Update quantity based on the current value of the price slider
+  price = priceSlider.value();
+  quantity = demandCurve(price);
+  
+  // Draw the axes
+  stroke(0);
+  strokeWeight(1);
+  line(margin, margin, margin, height - margin); // Vertical Y-axis
+  // Horizontal X axis
+  line(margin, height - margin, width - margin, height - margin);
+  strokeWeight(0);
+  text('Price', 30, 40); // Y-axis label
+  text('Quantity Sold', width - margin - 70, height - margin - 10); // X-axis label
+  
+  // Draw the demand curve
+  stroke(255, 0, 0);
+  strokeWeight(3);
+  line(margin, margin, width - margin, height - margin);
+  
+  // Draw a point on the demand curve based on current price and quantity
+  fill(0, 0, 255);
+  noStroke();
+  x = map(quantity, 0, maxQuantity, margin, height - margin)
+  y = map(price, 0, maxPrice, width - margin, margin)
+  circle(x,y,10);
+  
+  stroke('silver');
+  line(margin, y, x, y)
+  line(x, y, x, height - margin)
+  
+  fill(0);
+  noStroke();
+  circle(
+    margin, 
+    map(price, 0, maxPrice, width - margin, margin), 10);
+  
+  circle(
+    map(quantity, 0, maxQuantity, margin, height - margin), 
+    height - margin, 10);
+  
+  // Text annotations
+  noStroke();
+  fill(0);
+  textSize(16);
+  text("Price and Demand Curve", 120, 30);
+  text(`Input Price: ${price}`, 10, height - 20);
+  
+  text(`Output: Quantity = ${quantity}`, 70, 490);
+  textSize(12);
+  
+  // instructions
+  let keyMargin = 180;
+  text(`Adjust price by moving the slider.`,     keyMargin, margin);
+  text(`Note that as the input (price) changes`, keyMargin, margin + 20);
+  text(`    the quantity sold also changes.`,    keyMargin, margin + 40);
+  text(`At high prices a low quantity is sold.`, keyMargin, margin + 60);
+  text(`At low prices a high quantity is sold.`, keyMargin, margin + 80);
+}
